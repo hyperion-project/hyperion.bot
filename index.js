de224149c563961bc7098a084e415f8614cef4f2
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2026-01-02 10:05:36 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import pr_comment from "./src/pr_comment.js";
 import issues from "./src/issues.js";
-import trigger from "./src/trigger.js";
 
 export default (app) => {
   app.on(["pull_request.opened", "pull_request.synchronize"], pr_comment);
   app.on("issues.opened", issues);
-  app.on(["issue_comment.created", "issue_comment.edited"], trigger);
 };
